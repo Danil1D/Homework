67c41ad5a87d3765359a392dd05e81001afdbe4f
Danil_D <90185790+Danil1D@users.noreply.github.com>
2021-11-17 16:08:07 +0200

    Update lab.5(4).py

--- lab.5/lab.5(4).py
+++ lab.5/lab.5(4).py
@@ -1,12 +1,3 @@
-""" import random
-class Coloda():
-    def karta(self, suit, number ):
-        self.suit=suit
-        self.number=number
-    koloda = [None] * 37
-    def __init__(self) """
-
-
 import random 
  
 class Card:
